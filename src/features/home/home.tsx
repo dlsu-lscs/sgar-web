f9142bd8110312c323df51d1cbdad5d3c123262b
unknown <rohann_dizon@dlsu.edu.ph>
2025-09-19 16:25:44 +0800

    feat: adjusted components and added see more button to logo

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,26 +1,13 @@
-import Image from "next/image";
 import Navbar from "./components/navbar";
+import SgarLogo2 from "./components/sgar-logo";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col min-h-screen sm:px-30 sm:py-20 gap-8">
-        <div className="flex flex-col sm:w-[30%] w-full max-w-sm">
-          <Image
-            src="/assets/sgar_logo2.webp"
-            alt="SGAR Logo 2"
-            width={250}
-            height={250}
-            className="object-contain"
-          />
-          <p className="hidden sm:flex text-left text-xs font-light">
-            {" "}
-            DESCRIPTION: Neque porro quisquam est qui dolorem ipsum quia dolor
-            sit amet, consectetur, adipisci velit...{" "}
-          </p>
-        </div>
+      <main className="flex flex-col min-h-screen sm:px-30 sm:py-20 sm:items-start items-center">
+        <SgarLogo2 />
       </main>
     </>
   );
