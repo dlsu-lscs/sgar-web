c7a8a38a1c5514ef9e0d607e002080a235c42447
unknown <rohann_dizon@dlsu.edu.ph>
2025-09-19 20:58:40 +0800

    feat: added card for units, added lscs as placeholder

--- src/features/home/components/unitcard.tsx
+++ src/features/home/components/unitcard.tsx
@@ -6,8 +6,27 @@ interface Props {
   logo_url: string;
 }
 
-export default function UnitCard() {
+export default function UnitCard({ pub_url, title, logo_url }: Props) {
   return (
-    <div className="bg-[var(--card-foreground)] rounded-xs w-10 h-7">test</div>
+    <div
+      className="relative w-72 h-40 rounded-md shadow-md rounded-md bg-cover bg-center"
+      style={{ backgroundImage: `url(${pub_url})` }}
+    >
+      <div className={`absolute inset-0 rounded-md`} />
+
+      {/* increase opacity when needed */}
+      <div className="absolute inset-0 bg-black opacity-70 rounded-md" />
+
+      <div className="relative top-0 flex flex-col justify-between h-full p-2">
+        <Image
+          src={logo_url}
+          alt={"draft logo"}
+          width={60}
+          height={60}
+          className="object-contain"
+        />
+        <h3 className="text-md font-bold font-sans text-right">{title}</h3>
+      </div>
+    </div>
   );
 }
--- src/features/home/containers/department.tsx
+++ src/features/home/containers/department.tsx
@@ -1,8 +1,17 @@
 import UnitCard from "../components/unitcard";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import lscs from "../data/placeholder";
 
 interface Props {
   name: string;
 }
+
 export default function Dept({ name }: Props) {
   return (
     <div className="col-start-1 row-start-1 absolute inset-0 sm:top-165 top-115 w-full z-10">
@@ -10,7 +19,11 @@ export default function Dept({ name }: Props) {
         {name}
       </h2>
       <div className="flex flex-row px-5">
-        <UnitCard />
+        <UnitCard
+          pub_url={lscs.pub_url}
+          logo_url={lscs.logo_url}
+          title={lscs.title}
+        />
       </div>
     </div>
   );
