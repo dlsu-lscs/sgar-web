4a70112917217cc6eb8a54a4c8a46e883482cf70
unknown <rohann_dizon@dlsu.edu.ph>
2025-09-19 14:03:51 +0800

    feat: wow a black rectangle

--- src/features/home/components/navbar.tsx
+++ src/features/home/components/navbar.tsx
@@ -1,2 +1,18 @@
+import { Button } from "@/components/ui/button";
 
-export default function
+export default function Navbar() {
+  return (
+    <header>
+      <nav className="relative">
+        <li className="bg-black sm:flex text-sm justify-between items-center">
+          <ul>
+            <Button variant="link">HOME</Button>
+          </ul>
+          <ul>
+            <Button variant="link">MAP</Button>
+          </ul>
+        </li>
+      </nav>
+    </header>
+  );
+}
