2dc57af1a21de90f98d6f43e5b81a346fbf7b1ae
unknown <rohann_dizon@dlsu.edu.ph>
2025-09-20 23:42:45 +0800

    fix: missing scripts in package

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -17,9 +17,9 @@ export default function Home() {
         </div>
         <div className="absolute inset-0 sm:top-165 top-115 w-full">
           <div className="relative flex flex-col">
-            <DeptTop name={"MAIN COLLEGE UNITS"} delay={2000} />
-            <Dept name={"COMMISION AND OTHER UNITS"} delay={3000} />
-            <Dept name={"COMMISION AND OTHER UNITS"} delay={3000} />
+            <DeptTop name={"MAIN COLLEGE UNITS"} delay={3000} />
+            <Dept name={"COMMISION AND OTHER UNITS"} delay={5000} />
+            <Dept name={"COMMISION AND OTHER UNITS"} delay={7000} />
           </div>
         </div>
       </main>
