c413682288ba26a378584ce4448b6a2e66303adf
unknown <rohann_dizon@dlsu.edu.ph>
2025-09-22 19:04:36 +0800

    feat: added more boxes for unit description and pubs in unit page

--- src/features/unit/components/unit.tsx
+++ src/features/unit/components/unit.tsx
@@ -1,32 +1,45 @@
 import Image from "next/image";
+import ImageBox from "./unit/image-box";
 import { Button } from "@/components/ui/button";
 import lscs from "../data/placeholder";
 
 export default function Unit() {
   return (
-    <div className="bg-[var(--card-tertiary-foreground)] w-full h-200 flex relative flex-row items-top px-40">
+    <div className="bg-[var(--card-tertiary-foreground)] w-full h-200 flex relative flex-row items-top px-40 gap-5">
       <div className="w-70 h-120">
+        {/* application period box */}
         <div className="w-full h-20 border-2 border-white rounded-md items-center text-left p-5 mb-5">
           <p className="font-bold text-sm">APPLICATIONS OPEN UNTIL</p>
           <p className="text-sm">OCT 69, 2025</p>
         </div>
 
+        {/* organizational chart box */}
         <div className="w-full h-full border-2 border-white items-center text-left  relative flex flex-col">
-          <div className="w-full h-10 border-1 border-white font-bold text-center justify-center flex items-center overflow-hidden z-10">
+          <div className="w-full h-10 border-1 border-white font-bold text-sm text-center justify-center flex items-center overflow-hidden z-10">
             ORGANIZATIONAL CHART
           </div>
-          <div className="relative w-full h-full">
-            <Image
-              src="/assets/lscs_mainpub.webp"
-              fill
-              alt="Organizational Chart"
-              className="object-cover"
-            />
-            <div className="absolute inset-0 bg-black/50 items-center flex justify-center" />
-          </div>
+
+          {/* organizational chart pub component */}
+          <ImageBox
+            src="/assets/lscs_mainpub.webp"
+            alt="Organizational Chart"
+          />
         </div>
       </div>
-      <div className="w-70 h-102" />
+      <div className="w-150 h-145 relative">
+        {/* unit description section */}
+        <div className="w-full h-[50%] border-2 border-white rounded-md items-center text-left p-5 mb-5">
+          <h3 className="font-bold mb-3">UNIT DESCRIPTION</h3>
+          <p className="text-sm">{lscs.description} </p>
+        </div>
+        <div className="w-full h-67 border-2 border-white rounded-md items-center text-left mb-5">
+          {/* main pub component */}
+          <ImageBox
+            src="/assets/lscs_mainpub.webp"
+            alt="Organizational Chart"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/features/unit/data/placeholder.ts
+++ src/features/unit/data/placeholder.ts
@@ -2,6 +2,9 @@ const lscs = {
   pub_url: "/assets/lscs_mainpub.webp",
   logo_url: "/assets/lscs_logo1.webp",
   title: "La Salle Computer Society",
+  description:
+    "La Salle Computer Society (LSCS) is DLSU's largest and pioneering technological organization and the home organization of the College of Computer Studies. LSCS is committed to help students learn more about tech through interactive workshops, make new friends with recreational events, develop their leadership skills through the esteemed Junior Officer Training program, secure a job through various career events, and more. Now in its 40th year of service, LSCS continues to ignite, innovate, and inspire.",
+
   slug: "LSCS",
 };
 
